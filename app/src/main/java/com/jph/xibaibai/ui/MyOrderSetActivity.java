fe094964399fa4eab6ccdddfdd3dfed9e6556423
ForeverLover <516885817@qq.com>
2015-12-08 17:11:04 +0800

    新建我的订单列表

---
+++ app/src/main/java/com/jph/xibaibai/ui/MyOrderSetActivity.java
@@ -0,0 +1,15 @@
+package com.jph.xibaibai.ui;
+
+import com.jph.xibaibai.model.http.IAPIRequests;
+import com.jph.xibaibai.ui.activity.base.TitleActivity;
+
+/**
+ * 项目:XiBaiBai
+ * 作者：Hi-Templar
+ * 创建时间：2015/12/8 15:26
+ * 描述：$TODO
+ */
+public class MyOrderSetActivity extends TitleActivity{
+
+    private IAPIRequests apiRequests;
+}
